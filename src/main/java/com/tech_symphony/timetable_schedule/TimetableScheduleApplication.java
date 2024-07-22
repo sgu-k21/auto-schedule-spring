aba3c2d02f062cb2d8fa92d8d74d479c0199153d
1119-DuyNguyen <thanhduy191103@gmail.com>
2024-07-22 13:28:25 +0700

    setup project

---
+++ src/main/java/com/tech_symphony/timetable_schedule/TimetableScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.tech_symphony.timetable_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_symphony/timetable_schedule/TimetableScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_symphony.timetable_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
